a998069bfdd1c0651e5e0b5f5aaaab587fb719fb
anishudupa <anishgithub1@gmail.com>
2024-11-04 17:09:14 +0530

    update vercel.json and server.js

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
-import "dotenv/config"
-import { app } from "./app.js"
-import { dbConnect } from "./db/connect.js"
-function main() {
-    dbConnect().then(() => {
-        const { PORT } = process.env
-        app.listen(PORT, () => console.log(`app is listening on port ${PORT}`))
-    })
-}
+import "dotenv/config";
+import { app } from "./app.js";
+import { dbConnect } from "./db/connect.js";
 
-main()
+dbConnect();
+
+export default app;
